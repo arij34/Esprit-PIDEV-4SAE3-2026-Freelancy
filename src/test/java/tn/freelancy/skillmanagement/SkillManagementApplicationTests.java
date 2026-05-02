61fa01d7d1b490d20dbcca8ac41f6a47dbf6284a
arij34 <Arij.Achach@esprit.tn>
2026-05-02 18:46:40 +0100

    test: modifier unit tests

--- src/test/java/tn/freelancy/skillmanagement/SkillManagementApplicationTests.java
+++ src/test/java/tn/freelancy/skillmanagement/SkillManagementApplicationTests.java
@@ -1,13 +1,10 @@
 package tn.freelancy.skillmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SkillManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
